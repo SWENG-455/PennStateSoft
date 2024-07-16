63c0e53a152ee0d82b726b81102e4db41564f850
Griffin Apida <134345661+Griffinin@users.noreply.github.com>
2024-07-15 20:21:44 -0500

    Create Room.cs
    rough Room class

---
+++ PennStateSoft/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace PennStateSoft
+{
+    public class Room
+    {
+        private static int num = 0;
+        private int number = 0;
+        private bool special = false;
+        private Room(bool special)
+        {
+            num++;
+            this.special = special;
+        }
+    }
+}
